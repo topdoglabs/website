01a8366736622faabc5f36ec874354db7884b5a0
Matt Bruce <mbrucedogs@gmail.com>
2026-01-20 16:00:42 -0600

    smoother transisitons
    Signed-off-by: Matt Bruce <mbrucedogs@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Route, Routes } from "react-router-dom";
+import { ScrollToTop } from "./components/scroll-to-top.jsx";
 import { AppDetailPage } from "./pages/app-detail.jsx";
 import { AppsPage } from "./pages/apps.jsx";
 import { HomePage } from "./pages/home.jsx";
@@ -8,18 +9,21 @@ import { SupportPage } from "./pages/support.jsx";
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/apps" element={<AppsPage />} />
-      <Route path="/apps/:slug" element={<AppDetailPage />} />
-      <Route path="/support" element={<SupportPage />} />
-      <Route path="/privacy" element={<PrivacyPage />} />
-      <Route path="/about" element={<StaticPage slug="about" />} />
-      <Route path="/faq" element={<StaticPage slug="faq" />} />
-      <Route path="/terms" element={<StaticPage slug="terms" />} />
-      <Route path="/cookies" element={<StaticPage slug="cookies" />} />
-      <Route path="/connect" element={<StaticPage slug="connect" />} />
-      <Route path="*" element={<HomePage />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/apps" element={<AppsPage />} />
+        <Route path="/apps/:slug" element={<AppDetailPage />} />
+        <Route path="/support" element={<SupportPage />} />
+        <Route path="/privacy" element={<PrivacyPage />} />
+        <Route path="/about" element={<StaticPage slug="about" />} />
+        <Route path="/faq" element={<StaticPage slug="faq" />} />
+        <Route path="/terms" element={<StaticPage slug="terms" />} />
+        <Route path="/cookies" element={<StaticPage slug="cookies" />} />
+        <Route path="/connect" element={<StaticPage slug="connect" />} />
+        <Route path="*" element={<HomePage />} />
+      </Routes>
+    </>
   );
 };
